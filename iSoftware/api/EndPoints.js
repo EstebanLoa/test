a6618220d496e55e930d1492004a080dda626d44
Esteban Mateo Loachamin Aldaz <107655020+EstebanLoa@users.noreply.github.com>
2022-12-15 19:45:34 -0500

    new

--- iSoftware/api/EndPoints.js
+++ iSoftware/api/EndPoints.js
@@ -1,28 +1,7 @@
-const ProductController = require("../controller/Products");
-const UserController = require("../controller/Users");
-const InvoiceController = require("../controller/Invoices");
-const OrderController = require("../controller/Orders");
+const UniversityController = require("../controller/Universities");
 const express = require("express");
 const router = express.Router();
 
-router.post("/addProduct", ProductController.createProduct);
-router.get("/listProducts", ProductController.getProducts);
-router.put("/updateProduct", ProductController.updateProduct);
-router.delete("/deleteProduct", ProductController.deleteProduct);
-
-router.post("/addUser", UserController.createUser);
-router.get("/listUsers", UserController.getUsers);
-router.put("/updateUser", UserController.updateUser);
-router.delete("/deleteUser", UserController.deleteUser);
-
-router.post("/addInvoice", InvoiceController.createInvoice);
-router.get("/listInvoices", InvoiceController.getInvoices);
-router.put("/updateInvoice", InvoiceController.updateInvoice);
-router.delete("/deleteInvoice", InvoiceController.deleteInvoice);
-
-router.post("/addOrder", OrderController.createOrder);
-router.get("/listOrders", OrderController.getOrders);
-router.put("/updateOrder", OrderController.updateOrder);
-router.delete("/deleteOrder", OrderController.deleteOrder);
+router.get("/listUniversities", UniversityController.getUniversities);
 
 module.exports = router;
--- iSoftware/server.js
+++ iSoftware/server.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const portParemeter = 3012;
+const portParemeter = 3009;
 const EndPoints = require("./api/EndPoints");
 
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use("/iSoftware", EndPoints);
+app.use("/Test", EndPoints);
 app.get("/", (req, res) => {
   res.send("Team3 DevSolution");
 });
 mongoose.connect(
-  "mongodb+srv://root:navetech@isoftware.ua3y4od.mongodb.net/iSoftware?retryWrites=true&w=majority",
+  "mongodb+srv://root:navetech@isoftware.ua3y4od.mongodb.net/Test?retryWrites=true&w=majority",
   { useNewUrlParser: true },
   (err, res) => {
     err && console.log("Error al conectarse a la base de datos");
