58d8a667f25466633726d1eaf4bf61163c0f3559
Esteban Mateo Loachamin Aldaz <107655020+EstebanLoa@users.noreply.github.com>
2023-01-18 22:53:04 -0500

    Proyect

--- iSoftware/api/EndPoints.js
+++
@@ -1,7 +0,0 @@
-const UniversityController = require("../controller/Universities");
-const express = require("express");
-const router = express.Router();
-
-router.get("/listUniversities", UniversityController.getUniversities);
-
-module.exports = router;
